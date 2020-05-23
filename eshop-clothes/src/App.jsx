4bc86efcd75740a2e89ab972b80de4a46d76a730
spyrostheofilatos <spyros.theofilatos@gmail.com>
2020-05-23 13:27:29 +0300

    add header and links

--- eshop-clothes/src/App.jsx
+++ eshop-clothes/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import HomePage from './Pages/HomePage/HomePage';
 import {Route,Switch} from 'react-router-dom'
 import './App.css';
-import ShopPage from './Pages/Shop/Shop'
+import ShopPage from './Pages/Shop/Shop';
+import Header from './Components/Header/header';
 
 
 
@@ -12,6 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
+        <Header/>
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/shop" component={ShopPage}/>
